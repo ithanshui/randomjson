cff12b325261775367c3d538e9711023db9ab0b9
zhousheng <zhousheng@6002BB51C6AE47B.internal.baidu.com>
2017-03-21 16:34:00 +0800

    add

---
+++ index.js
@@ -0,0 +1,137 @@
+var randomNumber = require('random/number');
+var randomString = require('random/stirng');
+var randomNull = require('random/null');
+var randomBoolean = require('random/boolean');
+
+// json type
+var typeJson = ['number', 'string', 'null', 'boolean', 'array', 'object'];
+// custom type
+var customType = ['chinese', 'float', 'image', 'url', 'email', 'date', 'tel'];
+// Syntax Character
+// var syntaxCharacter = ['*', '+', '?', '[', ']', ',', '{@', '{', '}', '|', '-'];
+var syntaxCharacter = ['\\*', '\\+', '\\?', '\\[', '\\]', ',', '{@', '\\{', '\\}', '\\|', '\\-'];
+
+var typeKeys = typeJson.concat(customType);
+// syntaxKeys
+var syntaxKeys = typeJson.concat(customType);
+// or regexp
+var orTxtReg = /\|/;
+// Quantifier regexp
+var quanReg = /(\{(\d+)(,(\d*))?\})?/;
+// keyReg
+var keyReg = new RegExp('(' + typeKeys.join('|') + ')(?:\\{(\d+)(,(\d*))?\\})?');
+// optionalReg
+var optionalReg = new RegExp('(' + '\\[(.*?)\\]' + ')(?:\\{(\d+)(,(\d*))?\\})?');
+
+var randomJson = {};
+var syntaxSignReg = /\{@(.*)}/;
+// loop json
+function main(modelJson) {
+    readObject(modelJson, randomJson);
+}
+// read object
+function readObject(obj, upperObj) {
+    for (var p in obj) {
+        if (getJsonType(obj[p]) === 'array') {
+            upperObj[p] = [];
+            readArray(p, obj[p], upperObj[p]);
+        }
+        else if (getJsonType(obj[p]) === 'object') {
+            upperObj[p] = {};
+            readObject(p, obj[p], upperObj[p]);
+        }
+        else {
+            parseCharactor(p, obj[p], upperObj);
+        }
+    }
+}
+// read array
+function readArray(key, arr, upperObj) {
+    arr.forEach(function (item, index) {
+        if (getJsonType(item) === 'object') {
+            upperObj[index] = {};
+            readObject(index, item, upperObj[index]);
+        }
+        else if (getJsonType(item) === 'array') {
+            upperObj[index] = [];
+            readArray(index, item, upperObj[index]);
+        }
+        else {
+            parseCharactor(index, item, upperObj);
+        }
+    });
+}
+// parse charactors
+function parseCharactor(keyCha, valueCha, upperObj) {
+    var syntaxMatch = valueCha.match(syntaxSignReg);
+    var synTxt= '';
+    if (syntaxMatch !== null) {
+        synTxt = syntaxMatch[1];
+        handleTxt(synTxt);
+    }
+    else {
+        upperObj[keyCha] = valueCha;
+    }
+}
+function handleTxt(synTxt) {
+    if (orTxtReg.test(synTxt)) {
+        orTxt(synTxt);
+    }
+    else {
+        var keyRes = synTxt.match(keyReg);
+        var optionalRes = synTxt.match(optionalReg);
+        if (keyRes !== null) {
+            return randomByType(synTxt, keyRes[1], keyRes[2], keyRes[3], keyRes[4]);
+        }
+    }
+}
+function getTxtByKey(keyType, fNumber, sNumber, tNumber) {
+
+}
+// or txt
+function orTxt(synTxt) {
+
+}
+function randomByType(synTxt, keyType, fNumber, sNumber, tNumber) {
+    if (keyType === 'number') {
+        return randomNumber(fNumber, sNumber, tNumber);
+    }
+    else if (keyType === 'string') {
+        return randomString(fNumber, sNumber, tNumber);
+    }
+    else if (keyType === 'boolean') {
+        return randomBoolean();
+    }
+    else if (keyType === 'null') {
+        return randomNull();
+    }
+    else {
+        return synTxt;
+    }
+}
+// get json type
+getJsonType(arg1) {
+    if (arg1 === null) {
+        return 'null';
+    }
+    else if (typeof arg1 === 'string') {
+        return 'string';
+    }
+    else if (typeof arg1 === 'number') {
+        return 'number';
+    }
+    else if (typeof arg1 === 'boolean') {
+        return 'boolean';
+    }
+    else if (Object.prototype.toString.call(arg1) === '[object Array]') {
+        return 'array';
+    }
+    else if (Object.prototype.toString.call(arg1) === '[object Object]') {
+        return 'object';
+    }
+    else {
+        throw new Error('illegal type');
+    } 
+}
+
+module.exports = main;
---
+++ random/string.js
@@ -0,0 +1,37 @@
+var str = 'This Ecma Standard defines the ECMAScript 2016 Language. It is the seventh edition of the ECMAScript Language Specification. Since publication of the first edition in 1997, ECMAScript has grown to be one of the world\'s most widely used general purpose programming languages. It is best known as the language embedded in web browsers but has also been widely adopted for server and embedded applications.';
+var maxBits = 403;
+var generalFNumber = 5;
+var generalTNumber = 20;
+function randomString (fNumber, sNumber, tNumber) {
+	var count;
+	if (tNumber) {
+		count = Math.floor(fNumber + Math.random() * (tNumber - fNumber + 1));
+		if (str.length < count) {
+			str = concatStr(str, count);
+		}
+		return str.substring(0, count); 
+	}
+	else if (sNumber) {
+		count = Math.floor(fNumber + Math.random() * (maxBits - fNumber + 1));
+		return str.substring(0, count); 
+	}
+	else if (fNumber) {
+		if (str.length < fNumber) {
+			str = concatStr(str, fNumber);
+		}
+		return str.substring(0, fNumber); 
+	}
+	else {
+		count = Math.floor(generalFNumber + Math.random() * (generalTNumber - generalFNumber + 1));
+		return str.substring(0, count);
+	}
+}
+function concatStr(str, count) {
+	var len = str.length;
+	var needLen = Math.ceil((count - len) / len);
+	for (var i = 0; i < needLen; i++) {
+		str = str.concat(str);
+	}
+	return str;
+}
+module.exports = randomString;
